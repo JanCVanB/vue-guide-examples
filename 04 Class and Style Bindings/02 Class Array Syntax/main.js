a4b54605dc63a459bb221a350edd5a0ff9818992
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-08-31 02:10:07 -0700

    Add "Class and Style Bindings - Class Array Syntax"

---
+++ 04 Class and Style Bindings/02 Class Array Syntax/main.js
@@ -0,0 +1,25 @@
+var vm1 = new Vue({
+  el: '#example1',
+  data: {
+    activeClass: 'active',
+    errorClass: 'text-danger'
+  }
+})
+
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    activeClass: 'active',
+    errorClass: 'text-danger',
+    isActive: true
+  }
+})
+
+var vm3 = new Vue({
+  el: '#example3',
+  data: {
+    activeClass: 'active',
+    errorClass: 'text-danger',
+    isActive: true
+  }
+})
