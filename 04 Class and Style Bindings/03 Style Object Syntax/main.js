e47d52ed3f36f92e2e199223f0936e3269f670d0
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-08-31 02:17:41 -0700

    Add "Class and Style Bindings - Style Object Syntax"

---
+++ 04 Class and Style Bindings/03 Style Object Syntax/main.js
@@ -0,0 +1,17 @@
+var vm1 = new Vue({
+  el: '#example1',
+  data: {
+    activeColor: 'red',
+    fontSize: 30
+  }
+})
+
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    styleObject: {
+      color: 'red',
+      fontSize: '13px'
+    }
+  }
+})
