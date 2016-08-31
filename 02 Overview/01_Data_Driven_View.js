1bf832955f5a8ef336c9d3f49fb139e0c0a6c092
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-08-30 20:28:14 -0700

    Add "Overview - Data Driven View"

---
+++ 02 Overview/01_Data_Driven_View.js
@@ -0,0 +1,11 @@
+// this is our Model
+var exampleData = {
+  name: 'Vue.js'
+}
+
+// create a Vue instance, or, a "ViewModel"
+// which links the View and the Model
+var exampleVM = new Vue({
+  el: '#example-1',
+  data: exampleData
+})
